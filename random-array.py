4c3b2d6e6ca866b003f22e12b0360953a1bd460b
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2017-10-16 18:09:41 +0200

    Random array of length 365

---
+++ random-array.py
@@ -0,0 +1,8 @@
+from random import uniform
+random_number_controlled = uniform(20, 180)
+rounded = round(random_number_controlled)   
+print(rounded)
+random_array = [round(uniform(20, 180)) for _ in range(364)]
+print(random_array)
+
+print(random_array[0])
