d862e7f4321141c68cf8259e41d17357d142b2d1
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2017-10-18 18:07:54 +0200

    keeping the second version

--- depth_first_traversal_2.py
+++ depth_first_traversal_2.py
@@ -34,8 +34,21 @@ def find_all_paths(graph, start, end, path=[]):
             if node not in path:
                 newpaths = find_all_paths(graph, node, end, path)
                 for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+
+
+def find_all_paths2(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if not graph.has_key(start):
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path:
+                newpaths = find_all_paths(graph, node, end, path)
+                for newpath in newpaths:
                 	if len(newpath) == len(graph):
                 		paths.append(newpath)
         return paths
-
-# print(find_all_paths(graph,"A","D"))
