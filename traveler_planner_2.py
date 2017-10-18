151bfb8123ab65f678b54838c61bfe0427e86f1e
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2017-10-18 18:19:45 +0200

    renaming testing.py because it is working
    TODO : refactoring + changing output in couples

--- traveler_planner_2.py
+++ traveler_planner_2.py
@@ -1,21 +1,16 @@
 from combinations import printCombinations
 from combinations_on_list import printCombinationsArray
-from depth_first_traversal_2 import find_all_paths
+from depth_first_traversal_2 import find_all_paths2
 
 graph = {'MADRID': ['GRECE','LISBONNE'],
              'GRECE': ['LISBONNE','MADRID'],
              'LISBONNE': ['GRECE','MADRID']}
 
 
-# print(find_all_paths(graph,"MADRID","LISBONNE"))
-
-# print(find_all_paths(graph,"MADRID","GRECE"))
-
 keys = ["MADRID","LISBONNE","GRECE"]
 i = 1
 for n in printCombinationsArray(keys):
 	s = list(n)[0]
 	d= list(n)[1]
-	#print("Following are all different paths from %d to %d :" %(s, d))
-	print(find_all_paths(graph,s, d))
+	print(find_all_paths2(graph,s, d))
 i += 1
