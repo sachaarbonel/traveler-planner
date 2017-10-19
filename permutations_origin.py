e6b99964cd662a037e148789bd03a1ceaa4984f3
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2017-10-19 21:53:00 +0200

    beta working + permutations origin working
    TODO : change output of depth first traversal 2

--- permutations_origin.py
+++ permutations_origin.py
@@ -1,8 +1,9 @@
-a = [5, 66, 7, 8]
+def printPermutationsOrigin(array):
+	for i in range(1, len(array)):
+		yield array[i-1],array[i]
+	yield array[len(array)-1],array[0]
 
-print(a)
-
-for i in range(1, len(a)):
-		print(a[i-1],a[i])
-		print(a[len(a)-1],a[0])
-
+# i=1
+# for n in printPermutationsOrigin([5, 66, 7, 8]):
+# 	print(list(n))
+# i += 1
