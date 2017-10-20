56d5c149b2046a76dee53498b2815e1f9cf54465
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2017-10-20 20:11:52 +0200

    found an other implementation
    NOT sure yet if it is good

--- depth_first_traversal_2.py
+++ depth_first_traversal_2.py
@@ -24,6 +24,64 @@ def find_all_paths2(graph, start, end, path=[]):
             if node not in path:
                 newpaths = find_all_paths2(graph, node, end, path)
                 for newpath in newpaths:
+                	#print("test",newpath)
                 	if len(newpath) == len(graph):
                 		paths.append(newpath)
+
         return paths
+
+
+#euler
+
+def cyclic(g):
+    """Return True if the directed graph g has a cycle.
+    g must be represented as a dictionary mapping vertices to
+    iterables of neighbouring vertices. For example:
+
+    >>> cyclic({1: (2,), 2: (3,), 3: (1,)})
+    True
+    >>> cyclic({1: (2,), 2: (3,), 3: (4,)})
+    False
+
+    """
+    path = set()
+
+    def visit(vertex):
+        path.add(vertex)
+        for neighbour in g.get(vertex, ()):
+            if neighbour in path or visit(neighbour):
+                return True
+        path.remove(vertex)
+        return False
+
+    return any(visit(v) for v in g)
+
+
+
+graph = {'MADRID': ['GRECE','LISBONNE'],
+             'GRECE': ['LISBONNE'],
+             'LISBONNE': ['GRECE','MADRID']}
+
+
+key1=["MADRID","LISBONNE","GRECE"]
+
+
+# print(find_all_paths2(graph,key1[0],key1[2]))
+# print(find_all_paths2(graph,key1[0],key1[1]))
+# print("MADRID" in graph["GRECE"])
+print(cyclic(graph))
+
+# def find_all_paths3(graph, start, end, path=[]):
+#         path = path + [start]
+#         if start == end:
+#             return [path]
+#         if start not in graph:
+#         	return []
+#         paths = []
+#         for node in graph[start]:
+#             if node not in path:
+#                 newpaths = find_all_paths2(graph, node, end, path)
+#                 for newpath in newpaths:
+#                 	if len(newpath) == len(graph):
+#                 		paths.append(newpath)
+#         return paths
